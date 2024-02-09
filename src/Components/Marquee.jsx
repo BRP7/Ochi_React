3a927353dece245da95b6f3eb37c4ba132a389d2
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-02-09 23:51:12 +0530

    Marquee

--- src/Components/Marquee.jsx
+++ src/Components/Marquee.jsx
@@ -1,9 +1,14 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 function Marquee() {
   return (
-    <div className='w-full h-screen bg-[#004D43]'>
-      
+    <div className='w-full py-10 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden gap-11 ">
+        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:5}} className='text-[13vw] leading-none uppercase pt-1 pb-2 font-semibold'>We Are Ochi</motion.h1>
+        <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:5}} className='text-[13vw] leading-none uppercase pt-1 pb-2 font-semibold'>We Are Ochi</motion.h1>
+        {/* <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:5}} className='text-[13vw] leading-none uppercase pt-1 pb-2 font-semibold'>We Are Ochi</motion.h1> */}
+      </div>
     </div>
   )
 }
