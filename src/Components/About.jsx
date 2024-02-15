669741bcf5e90c134857318dcbda866b8926311a
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-02-15 13:34:26 +0530

    About Section

---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black'>
+        <h1 className='font-["Neue_Montreal"] text-[4vw] tracking-tight leading-[4.5vw]'>Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.</h1>
+         <div className="w-full border-t-[1px] bg-[#676a5a]"></div>
+    </div>
+  )
+}
+
+export default About
