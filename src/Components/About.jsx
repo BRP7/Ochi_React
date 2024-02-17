461eb5990cdab4f54040d0198a997187f03377c5
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-02-17 18:52:15 +0530

    Eyes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 function About() {
   return (
     <div className='w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black'>
-        <h1 className='font-["Neue_Montreal"] text-[4vw] tracking-tight leading-[4.5vw]'>Ochi is a strategic partner for fast-grow­ing tech businesses that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and hire great peo­ple.</h1>
-         <div className="w-full border-t-[1px] bg-[#676a5a]"></div>
+        <h1 className='font-["Neue_Montreal"] text-[4vw] tracking-tight leading-[4.5vw]'>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell prod­ucts, explain com­plex ideas, and hire great peo­ple.</h1>
+         <div className="w-full flex gap-5 border-t-[1px] mt-20  border-[#a1b562]">
+          <div className="w-1/2 mt-10">
+            <h1 className="text-7xl">Our approach:</h1>
+             <button className='flex uppercase gap-10 text-white items-center px-10 py-6 bg-zinc-900 mt-10 rounded-full'>Read More
+                <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
+             </button>
+          </div>
+          <div className=" w-1/2 h-[70vh] rounded-3xl  mt-10"><img class="bg-gray-200  w-full max-w-full rounded entered loaded" data-component="lazyload" data-animate="data-animate" src="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg" width="663" height="469" data-src="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg" data-srcset="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg 1x, https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-1326x939.jpg 2x" alt="image description" data-ll-status="loaded" srcset="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg 1x, https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-1326x939.jpg 2x" /></div>
+         </div>
     </div>
   )
 }
---
+++ src/Components/Eyes.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+
+function Eyes() {
+
+    const [rotate,setRotate] = useState(0);
+
+   useEffect(()=>{
+    window.addEventListener("mousemove",(e)=>{
+        let mouseX=e.clientX;
+        let mouseY=e.clientY;
+
+        let deltaX = mouseX - window.innerWidth/2;
+        let deltaY = mouseY - window.innerHeight/2;
+
+        let angle = Math.atan2(deltaY,deltaX)*(180/Math.PI);
+        setRotate(angle);
+    })
+   })
+
+  return (
+    <div className='eyes w-full h-screen overflow-hidden'>
+        <div className='relative w-full h-screen'>
+        <img className="bg-gray-200 max-w-full w-full h-full object-cover transform scale-120 entered loaded" data-component="lazyload" src="https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg" width="1440" height="921" data-src="https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg" data-srcset="https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg 1x, https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg 2x" alt="image description" data-ll-status="loaded" srcset="https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg 1x, https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg 2x" />
+        <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] flex items-center gap-10 ">
+            <div className="flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full">
+            <div className="relative w-2/3 h-2/3 bg-zinc-900 rounded-full">
+                <div style={{transform:`translate(-50%,-50%) rotate(${rotate})`}} className="line w-full h-10 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] ">
+                     <div className="w-7 h-7 bg-zinc-100 rounded-full"></div>
+            </div>
+            </div>
+            </div>
+            <div className="flex items-center justify-center w-[15vw] h-[15vw] bg-zinc-100 rounded-full">
+                <div className="relative w-2/3 h-2/3 bg-zinc-900 rounded-full">  
+                <div style={{transform:`translate(-50%,-50%) rotate(${rotate})`}} className="line w-full h-10 absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] ">
+                     <div className="w-7 h-7 bg-zinc-100 rounded-full"></div>
+            </div>
+            </div>
+         </div>
+     
+ </div>
+    </div>
+    </div>
+  )
+}
+
+export default Eyes
